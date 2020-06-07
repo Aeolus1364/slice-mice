c224db9d2af8e3d5cf1792703b397287c7457e55
Aeolus1364 <thetesseractsshadow@gmail.com>
2020-06-07 13:50:55 -0400

    added triangle class, and implemented mutli poly ray intersection

--- slice-mice/Main.cpp
+++ slice-mice/Main.cpp
@@ -9,8 +9,8 @@
 
 #include "shader.h"
 
-const int width = 1920;
-const int height = 1080;
+const int width = 800;
+const int height = 600;
 
 const float background[] = {0.647f, 0.533f, 0.333f};
 
@@ -19,6 +19,8 @@ float last_frame = 0.0f;
 
 double last_x = 0;
 double last_y = 0;
+
+bool pause = false;
 
 glm::mat4 projection, view;
 
@@ -68,7 +70,7 @@ float unit_cube[] = {
 };
 
 float triangle[] = {
-	0.0f, 1.0f, 0.0f,
+	0.0f, 1.0f, 0.5f,
 	0.0f, 0.0f, 0.5f,
 	0.0f, 0.0f, -0.5f
 };
@@ -82,14 +84,78 @@ float unit_line[] = {
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, int button, int action, int modifier);
 glm::vec3 get_mouse_loc(GLFWwindow* window, float depth);
+float mag(glm::vec3 vector);
 
 //remove
 void processInput(GLFWwindow* window);
+
+class Triangle {
+	glm::vec4 m_verts[3];
+	glm::mat4* m_model;
+	glm::vec3 m_intersection;
+
+public:
+	Triangle() {
+
+	}
+	Triangle(float* vertices, glm::mat4* model) {
+		m_verts[0] = glm::vec4(vertices[0], vertices[1], vertices[2], 0.0f);
+		m_verts[1] = glm::vec4(vertices[3], vertices[4], vertices[5], 0.0f);
+		m_verts[2] = glm::vec4(vertices[6], vertices[7], vertices[8], 0.0f);
+
+		m_model = model;
+
+		m_intersection = glm::vec3(0.0f);
+	}
+	bool ray_intersection(glm::vec3 ray_pos, glm::vec3 ray_vector) {
+		// converting from local coordinates to world coordinates
+		glm::vec3 a_world = *m_model * m_verts[0];
+		glm::vec3 b_world = *m_model * m_verts[1];
+		glm::vec3 c_world = *m_model * m_verts[2];
+
+		// calculates the vectors for each side of the triangle
+		glm::vec3 ab = b_world - a_world;
+		glm::vec3 ca = a_world - c_world;
+		glm::vec3 bc = c_world - b_world;
+
+		// normal vector calculated by crossing two triangle vectors
+		glm::vec3 n = glm::cross(ca, ab);
+
+		// w is the vector from the ray's to the point defining the plane
+		glm::vec3 w = a_world - ray_pos;
+
+		// calculates the ray's intersection with the plane
+		glm::vec3 intersect = ray_pos + (glm::dot(w, n) / glm::dot(ray_vector, n)) * ray_vector;
+
+		// calculates vectors pointing from the intersection point to each point on the triangle
+		glm::vec3 ia = intersect - a_world;
+		glm::vec3 ib = intersect - b_world;
+		glm::vec3 ic = intersect - c_world;
+
+		// calculates 3 values that are positive or negative based on which side of each line the point is on
+		float side_ab = glm::dot(glm::cross(ab, ia), n);
+		float side_bc = glm::dot(glm::cross(bc, ib), n);
+		float side_ca = glm::dot(glm::cross(ca, ic), n);
+
+		// saves intersect point
+		m_intersection = intersect;
+
+		// if the values are positive for every side, the point is inside the triangle
+		return (side_ab >= 0 && side_bc >= 0 && side_ca >= 0);
+	}
+
+	glm::vec3 get_intersect() {
+		return m_intersection;
+	}
+};
 
 class Object {
 	unsigned int m_VAO, m_VBO;
 	unsigned int m_num_verts;
 	bool m_rotate = false;
+
+	Triangle m_polys[100];
+	unsigned int m_num_polys;
 
 	glm::mat4 m_model = glm::mat4(1.0f);
 
@@ -112,6 +178,22 @@ public:
 
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(0);
+
+		m_num_polys = m_num_verts / 3;
+
+		std::cout << "Num verts: " << m_num_verts << std::endl;
+		std::cout << "Num polys: " << m_num_polys << std::endl;
+
+		for (unsigned int i = 0; i < m_num_polys; ++i) {
+			std::cout << "Generated triangle " << i << std::endl;
+			float* v_chunk = verts + i * 9;
+			std::cout << "P1: " << v_chunk[0] << ", " << v_chunk[1] << ", " << v_chunk[2] << std::endl;
+			std::cout << "P2: " << v_chunk[3] << ", " << v_chunk[4] << ", " << v_chunk[5] << std::endl;
+			std::cout << "P3: " << v_chunk[6] << ", " << v_chunk[7] << ", " << v_chunk[8] << std::endl;
+
+			m_polys[i] = Triangle(v_chunk, &m_model);
+		}
+
 	}
 	void activate() {
 		glBindVertexArray(m_VAO);
@@ -123,6 +205,7 @@ public:
 		if (m_rotate) {
 			m_model = glm::rotate(m_model, (float)glfwGetTime() / 3, glm::vec3(0.0f, 1.0f, 0.0f));
 		}
+		//m_model = glm::rotate(m_model, 0.5f, glm::vec3(0.0f, 1.0f, 0.0f));
 		m_model = glm::scale(m_model, m_scale);
 
 	}
@@ -131,6 +214,18 @@ public:
 		shader.setMat4("model", m_model);
 		shader.setVec3("color", m_color);
 		glDrawArrays(GL_TRIANGLES, 0, m_num_verts);
+	}
+	bool intersect(glm::vec3 ray_pos, glm::vec3 ray_vector) {
+		bool intersected = false;
+
+		// iterates through all the triangles in the object
+		for (unsigned int i = 0; i < m_num_polys; ++i) {
+			if (m_polys[i].ray_intersection(ray_pos, ray_vector)) {
+				intersected = true;
+				break;
+			}
+		}
+		return intersected;
 	}
 	void translate(glm::vec3 translate) {
 		m_translation = translate;
@@ -165,8 +260,6 @@ public:
 		glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
 		glBufferData(GL_ARRAY_BUFFER, 24, verts, GL_STATIC_DRAW);
 
-		std::cout << sizeof(verts) << std::endl;
-
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(0);
 	}
@@ -177,7 +270,7 @@ public:
 		m_model = glm::mat4(1.0f);
 
 		m_model = glm::translate(m_model, m_translation);
-		m_model = glm::scale(m_model, glm::vec3(10.0f));
+		m_model = glm::scale(m_model, glm::vec3(3.0f));
 	}
 	void draw(Shader shader) {
 		activate();
@@ -196,7 +289,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(width, height, "Slice Mice", glfwGetPrimaryMonitor(), NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "Slice Mice", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
@@ -216,24 +309,27 @@ int main() {
 
 	Shader shader("vertex.glsl", "fragment.glsl");
 
-	Object test(unit_cube, 36);
-	Object tri(triangle, 9);
+	float quad_verts[] = {
+		0.0f, -1.0f, 1.0f,
+		0.0f, 1.0f, -1.0f,
+		0.0f, -1.0f, -1.0f,
+
+		0.0f, -1.0f, 1.0f,
+		0.0f, 1.0f, -1.0f,
+		0.0f, 1.0f, 1.0f
+	};
+
+	Object quad(unit_cube, 36);
+
 	Line ln(unit_line);
 
 	shader.use();
-
-	glm::vec4 a = glm::vec4(triangle[0], triangle[1], triangle[2], 0.0f);
-	glm::vec4 b = glm::vec4(triangle[3], triangle[4], triangle[5], 0.0f);
-	glm::vec4 c = glm::vec4(triangle[6], triangle[7], triangle[8], 0.0f);
 	
 
-	glm::vec3 v = glm::vec3(-1.0f, 0.0f, 0.0f);
-
-
+	glm::vec3 ray_vector = glm::vec3(-1.0f, 0.0f, 0.0f);
 
 	//								   fov					aspect ratio	  near   far
 	projection = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
-	//projection = glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, 0.1f, 100.0f);
 	shader.setMat4("projection", projection);
 
 	//					    position				  target					up
@@ -255,45 +351,18 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
 		shader.use();
+			
+		glm::vec3 ray_pos = get_mouse_loc(window, 3.0f);
+			
+		quad.rotate(true);
 
-		tri.rotate(true);
-		tri.scale(glm::vec3(2.0f));
-		tri.update();
-
-		glm::mat4 trimodel = tri.get_model();
-		
-		glm::vec3 a_world = trimodel * a;
-		glm::vec3 b_world = trimodel * b;
-		glm::vec3 c_world = trimodel * c;
-
-		glm::vec3 ab = b_world - a_world;
-		glm::vec3 ac = c_world - a_world;
-		glm::vec3 n = glm::cross(ab, ac);
-
-		glm::vec3 pos = get_mouse_loc(window, 2.5f);
-
-		glm::vec3 w = pos - c_world;
-
-		glm::vec3 intersect = pos + (glm::dot(w, n) / glm::dot(v, n)) * v;
-		std::cout << intersect[0] << ", " << intersect[1] << ", " << intersect[2] << std::endl;
-
-
-		ln.translate(pos);
-
+		ln.translate(ray_pos);
 		ln.update();
 		ln.draw(shader);
 
-		
-		tri.draw(shader);
+		quad.update();
+		quad.draw(shader);
 
-		test.rotate(true);
-
-		test.translate(glm::vec3(5 - intersect[0], intersect[1], intersect[2]));
-		test.scale(glm::vec3(0.25));
-		test.color(glm::vec3(1.0f));
-
-		test.update();
-		test.draw(shader);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -336,4 +405,15 @@ void mouse_callback(GLFWwindow* window, int button, int action, int modifier) {
 void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+	pause = (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS);
+
+}
+
+float mag(glm::vec3 vector) {
+	// simple 3D pythagorean theorem calculator
+	float a = vector[0];
+	float b = vector[1];
+	float c = vector[2];
+
+	return pow(a * a + b * b + c * c, 0.5);
 }
